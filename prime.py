62e9b2cfb1785fa592cc0841ea32c77c6d9510cc
Khawlaowl <138932395+Khawlaowl@users.noreply.github.com>
2023-11-26 21:04:27 +0100

    f

--- prime.py
+++ prime.py
@@ -34,31 +34,18 @@ def gen_primes(n):
     q = 2
     
     while q<n:
+        q = 2
+    
+    while True:
         if q not in D:
-            # q is a new prime.
-            # Yield it and mark its first multiple that isn't
-            # already marked in previous iterations
-            # 
-            #yield q
             D[q * q] = [q]
+            yield q
         else:
-            # q is composite. D[q] is the list of primes that
-            # divide it. Since we've reached q, we no longer
-            # need it in the map, but we'll mark the next 
-            # multiples of its witnesses to prepare for larger
-            # numbers
-            # 
             for p in D[q]:
                 D.setdefault(p + q, []).append(p)
             del D[q]
-        if n % int(q) == 0:
-            yield q
-            break
-        
-        
+
         q += 1
-
-
 
 def factor(n, lp):
     if n in lp:
